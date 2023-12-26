bbf1ddcf4ac57a85885a279883964083ca34f3eb
Administrator <Administrator@304-18>
2023-12-26 14:56:52 +0900

    2023.12.26

---
+++ src/main/java/com/nowon/choi/Springboot3CicdTest02Application.java
@@ -0,0 +1,13 @@
+package com.nowon.choi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3CicdTest02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3CicdTest02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nowon/choi/Springboot3CicdTest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowon.choi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3CicdTest02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
